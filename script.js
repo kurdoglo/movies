10421ea825ac3f8074b32866b796ad076a716563
VItaliy <pupkin@mail.ru>
2024-07-27 11:08:16 +0300

    conditions and loop

--- script.js
+++ script.js
@@ -25,11 +25,6 @@
 const answer = +prompt("Сколько фильмов вы уже посмотрели?", "");
 const numberOfFilms = answer;
 
-let a = prompt("Один из последних просмотренных фильмов?", "");
-let b = prompt("На сколько оцените его?", "");
-let c = prompt("Один из последних просмотренных фильмов?", "");
-let d = prompt("На сколько оцените его?", "");
-
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -37,9 +32,66 @@ const personalMovieDB = {
   genres: [],
   private: false,
 };
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", "");
+  const b = prompt("На сколько оцените его?", "");
+
+  if (a != "" && b != "" && a != null && b != null && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    --i;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Error"); 
+}
 
 console.log(numberOfFilms);
 
 console.log(personalMovieDB.movies);
+
+// // const num = 50;
+
+// num == 50 ? console.log("ok") : console.log("not ok");
+
+// switch (num) {
+//   case 50:
+//     console.log("okay");
+//     break;
+//   case 49:
+//     console.log("not good");
+//     break;
+//   case 45:
+//     console.log("no");
+//     break;
+//   case 43:
+//     console.log("noo");
+//     break;
+// }
+
+// let num = 50;
+// // while (numb < 56) {
+// //   console.log(numb);
+// //   numb++;
+// // }
+
+// // do {
+// //   console.log(numb);
+// //   numb++;
+// // } while (numb < 56);
+
+// for (let i = 1; i < 10; i++) {
+//   if (i === 6) {
+//     break;
+//   }
+//   num++;
+//   console.log(num);
+// }
